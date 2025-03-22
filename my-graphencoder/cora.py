f6df4e68ae8c6231c5b93313da86a3f94d0115b8
bwcd-123 <21009200114@stu.xidian.edu.cn>
2025-03-22 16:52:00 +0800

    dd

--- my-graphencoder/cora.py
+++ my-graphencoder/cora.py
@@ -3,53 +3,56 @@ from torch_geometric.datasets import Planetoid
 from torch_geometric.utils import to_dense_adj
 from sklearn.cluster import KMeans
 import swanlab
+import argparse
 
 from model import StackedEncoder
 from utils import pretrain_layers, fine_tune, eval
 
-data_root = '../dataset'
 data_name = 'Cora'
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-dataset = Planetoid(root=data_root, name=data_name)
-data = dataset[0].to(device)  # 将数据移到设备
 layer_dims = [2708, 1024, 512, 128]
 sparsity_params = {'sparsity_target': 0.1, 'sparsity_weight': 0.3}
 epochs_per_layer=100
 epochs=100
-random_state=42
 
-torch.manual_seed(random_state)
-swanlab.init(
-    project="graph-encoder",
-    experiment_name=f"ge-{data_name}",
-    config={
-        'layer_dims': layer_dims,
-        'sparsity_params': sparsity_params,
-        'data_root': data_root,
-        'data_name': data_name,
-        'epochs_per_layer': epochs_per_layer,
-        'epochs': epochs,
-        'random_state': random_state,
-        'seed': random_state,
-        'device': device
-    },
-    mode="cloud"
-)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_name', type=str, default=data_name)
+    parser.add_argument('--layer_dims', type=list, default=layer_dims)
+    parser.add_argument('--sparsity_params', type=dict, default=sparsity_params)
+    parser.add_argument('--epochs_per_layer', type=int, default=epochs_per_layer)
+    parser.add_argument('--epochs', type=int, default=epochs)
+    args = parser.parse_args()
+    args.seed=torch.default_generator.seed()
+    args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    dataset = Planetoid(root='../dataset', name=data_name)
+    kmeans = KMeans(n_clusters=dataset.num_classes)
+    args.kmeans_seed = kmeans.random_state
+    data = dataset[0].to(args.device)  # 将数据移到设备
 
-# 构建归一化相似矩阵（保持在GPU）
-adj_matrix = to_dense_adj(data.edge_index)[0].squeeze().to(device)
-degrees = adj_matrix.sum(dim=1)
-D_inv = torch.diag(1.0 / degrees.clamp(min=1e-12)).to(device)
-normalized_S = (D_inv @ adj_matrix).detach()  # 确保不计算梯度
+    swanlab.init(
+        project=f"graph-encoder-{args.data_name}",
+        experiment_name=f"ge-{data_name}",
+        config=vars(args),
+        mode="cloud"
+    )
 
-model = StackedEncoder(layer_dims, sparsity_params).to(device)
+    # 构建归一化相似矩阵（保持在GPU）
+    adj_matrix = to_dense_adj(data.edge_index)[0].squeeze().to(args.device)
+    degrees = adj_matrix.sum(dim=1)
+    D_inv = torch.diag(1.0 / degrees.clamp(min=1e-12)).to(args.device)
+    normalized_S = (D_inv @ adj_matrix).detach()  # 确保不计算梯度
 
-# 训练阶段
-model = pretrain_layers(model, normalized_S, epochs_per_layer, device)
-final_embedding = fine_tune(model, normalized_S, epochs, device)
+    model = StackedEncoder(layer_dims, sparsity_params).to(args.device)
 
-# 评估（确保数据在CPU）
-embedding_np = final_embedding.numpy()
-kmeans = KMeans(n_clusters=dataset.num_classes, random_state=random_state)
-labels_pred = kmeans.fit_predict(embedding_np)
-eval(data.y.cpu().numpy(), labels_pred)
+    # 训练阶段
+    model = pretrain_layers(model, normalized_S, epochs_per_layer, args.device)
+    final_embedding = fine_tune(model, normalized_S, epochs, args.device)
+
+    # 评估（确保数据在CPU）
+    embedding_np = final_embedding.numpy()
+    
+    labels_pred = kmeans.fit_predict(embedding_np)
+    acc, ari_score, nmi_score, homogeneity, completeness, v_measure = eval(data.y.cpu().numpy(), labels_pred)
+    swanlab.log({"acc": acc, "ari_score": ari_score, "nmi_score": nmi_score, "homogeneity": homogeneity,
+                 "completeness": completeness, "v_measure": v_measure})
+    swanlab.finish()
