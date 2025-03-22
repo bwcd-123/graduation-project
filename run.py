32971d099e9f9454fb293844600c7af977c27227
bwcd-123 <21009200114@stu.xidian.edu.cn>
2025-03-22 16:55:54 +0800

    dd:

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ swanlab.login(api_key="1REF3sznzagTLaWp98nEZ")
 def task():
     for i in range(n):
         os.system("python my-graphencoder/cora.py --data_name Citeseer")
+        os.system("python my-graphencoder/cora.py --data_name Pubmed")
 
 if __name__ == "__main__":
     # 创建 4 个进程
